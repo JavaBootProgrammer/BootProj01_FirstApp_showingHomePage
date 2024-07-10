bb5e263f1188ab85f30d16cde63886c6c3ae96b7
apasunoori <anand8533@gmail.com>
2024-07-10 16:49:53 -0400

    1

---
+++ src/main/java/com/mvc/demo/BootProj01FirstAppShowingHomePageApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProj01FirstAppShowingHomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProj01FirstAppShowingHomePageApplication.class, args);
+	}
+
+}
